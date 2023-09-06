35af3b8e76a540520031ced68ba2512bc1885005
emilio yoc <emilioyoc@Emilios-MacBook-Pro.local>
2023-09-06 11:43:37 -0600

    component activity indicator

--- App.js
+++ App.js
@@ -1,23 +1,11 @@
-import React, {useState} from 'react';
-import {View, Switch, Text} from 'react-native';
+import React from 'react';
+import { ActivityIndicator, View, Text } from 'react-native';
 
-const App = () => {
-  const [isEnabled, setIsEnabled] = useState(false);
+const LoadingScreen = () => (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text>porfavor espera</Text>
+    <ActivityIndicator size="large" color="#0000ff" />
+  </View>
+);
 
-  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
-
-  return (
-    <View>
-      <Text style={{marginTop:100}}>Enable Feature:</Text>
-      <Switch
-      style={{marginLeft:15}}
-      trackColor={{ false: "#767577", true: "#81b0ff" }}
-      thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
-      onValueChange={toggleSwitch}
-      value={isEnabled}
-      />
-    </View>
-  );
-};
-
-export default App;
+export default LoadingScreen;
