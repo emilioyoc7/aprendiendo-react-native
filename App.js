bcca9590233c12d1de1e6f4a93de013b0222c099
emilio yoc <emilioyoc@Emilios-MacBook-Pro.local>
2023-09-06 12:11:17 -0600

    component modal

--- App.js
+++ App.js
@@ -1,11 +1,43 @@
-import React from 'react';
-import { ActivityIndicator, View, Text } from 'react-native';
+import React, {useState} from 'react';
+import {Modal, Text, TouchableHighlight, View, Alert} from 'react-native';
 
-const LoadingScreen = () => (
-  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text>porfavor espera</Text>
-    <ActivityIndicator size="large" color="#0000ff" />
-  </View>
-);
+const App = () => {
+  const [modalVisible, setModalVisible] = useState(false);
 
-export default LoadingScreen;
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Modal
+        style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+        animationType="none"
+        transparent={false}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+          setModalVisible(!modalVisible);
+        }}>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <View>
+            <Text>hola, soy un Modal!</Text>
+
+            <TouchableHighlight
+              onPress={() => {
+                setModalVisible(!modalVisible);
+              }}>
+              <Text>borrar Modal</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+      </Modal>
+
+      <TouchableHighlight
+        style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+        onPress={() => {
+          setModalVisible(true);
+        }}>
+        <Text>mostrar Modal</Text>
+      </TouchableHighlight>
+    </View>
+  );
+};
+
+export default App;
